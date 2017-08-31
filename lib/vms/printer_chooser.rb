d615c2992df93399622a15fd69bfa6375a36cefc
Toby Varland <toby@tobyvarland.com>
2017-08-31 10:51:20 -0400

    Testing

--- lib/vms/printer_chooser.rb
+++ lib/vms/printer_chooser.rb
@@ -1,9 +1,17 @@
 module VMS
 
+  #
   class PrinterChooser
-    def self.hi
-      "Hello world from the printer chooser!"
+
+    def self.choose
+
+      return nil if request.nil?
+      return nil if request.remote_ip.nil?
+
+      return request.remote_ip
+
     end
+
   end
 
 end
--- vms.gemspec
+++ vms.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'vms'
-  s.version     = '0.0.1'
+  s.version     = '0.0.2'
   s.date        = '2017-08-31'
   s.summary     = "VMS Ruby Library"
   s.description = "VMS Ruby Library"
